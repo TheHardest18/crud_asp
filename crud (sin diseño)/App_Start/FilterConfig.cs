747f75481c962ef6873c3054bb9035de9a45b179
TheHardest18 <isias1626@gmail.com>
2021-09-12 10:00:33 -0400

    Agregar archivos de proyecto.

---
+++ crud (sin diseño)/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace crud__sin_diseño_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ crud (sin diseño)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="crud__sin_diseño_.MvcApplication" Language="C#" %>
